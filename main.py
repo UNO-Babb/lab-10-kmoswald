c1ac230fa88b5aba61337f91b45644349a5077d6
kmoswald <kaitlynoswald@unomaha.edu>
2025-11-17 03:05:27 +0000

    Done with lab

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 #MapPlot.py
-#Name:
-#Date:
-#Assignment:
+#Name:Kaitlyn Oswald
+#Date:11/16/25
+#Assignment: Lab 10
+
+
+import coffee
+import pandas
+import matplotlib.pyplot as plt
+
+coffee = coffee.get_coffee()
+
+
+#print(coffee[0]["Data"]["Scores"]["Total"])
+years = []
+scores = []
+for bean in coffee:
+    year = bean["Year"]
+    score = bean["Data"]["Scores"]["Total"]
+    if score != 0:
+        years.append(year)
+        scores.append(score)
+    #print(year,score)
+
+df = pandas.DataFrame({"Year": years,
+                       "Score": scores})
+
+
+print(df)
+df.plot(kind = 'scatter', x = 'Year', y = 'Score')
+
+plt.savefig("output")
